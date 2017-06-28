1679dbdaf19197d1c792545b4d151f54238363b4
Malte Schlüter <git@schlueter-malte.de>
2017-06-28 22:42:39 +0200

    [INIT] Add ServiceProvider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mschlueter\Backend;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
