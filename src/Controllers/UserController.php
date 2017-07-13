690f72e92cd4c831a540737e956ea116cbd03b12
Malte Schlüter <m.schlueter@ballyhoo.de>
2017-07-13 16:09:23 +0200

    [INIT] Edit user validation table

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -44,7 +44,7 @@ class UserController extends Controller {
 
         $this->validate($request, [
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'email' => 'required|string|email|max:255|unique:backend_users',
         ]);
 
         $password = str_random();
